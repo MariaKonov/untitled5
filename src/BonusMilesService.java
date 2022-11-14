596c41e6a2f0878181dc9c4ef6cc8f699efaaa6c
Maria <marijka000@gmail.com>
2022-11-14 19:59:40 +0300

    Change

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,6 @@
 public class BonusMilesService {
-    public int calculate(int prime){
-        int mile = 20;// рублей для одной бонусной мили
-        int bonus= prime/mile;
-        return bonus;
+    public int calculate(int prime) {
+        int miles = 20;// рублей для одной бонусной мили
+        return miles;
     }
 }
